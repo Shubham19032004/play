e8c18d2d0c1b41400102ce627ab16bdf00197b46
shubham <shubhamnainwal70@gmail.com>
2024-02-13 20:22:16 +0530

    models

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,90 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      lowecase: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+      lowecase: true,
+      trim: true,
+    },
+    coverimg: {
+      type: String,
+      lowecase: true,
+      trim: true,
+    },
+    watchhistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password);
+  next();
+});
+userSchema.method.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFESH_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
